d07ae446ed1757a9384de42c8e74af7046541a1a
phuoc759860 <phuochuynh120@gmail.com>
2026-07-29 10:49:59 +0700

    chore: gitignore dev/test files (SQL scripts, dotnet-tools, render.yaml)

--- fix_passwords.sql
+++
@@ -1,4 +0,0 @@
-UPDATE Users SET PasswordHash='$2b$12$ANTEBrfxRD6u43ZeV2k5N.IfvfSu.n9N9j/Kr3HBHxMb6.La0sIS6' WHERE Email='emcikay@gmail.com';
-UPDATE Users SET PasswordHash='$2b$12$ANTEBrfxRD6u43ZeV2k5N.IfvfSu.n9N9j/Kr3HBHxMb6.La0sIS6' WHERE Email='hacker@gmail.com';
-UPDATE Users SET PasswordHash='$2b$12$ANTEBrfxRD6u43ZeV2k5N.IfvfSu.n9N9j/Kr3HBHxMb6.La0sIS6' WHERE Email='A@gmail.com';
-UPDATE Users SET PasswordHash='$2b$12$ANTEBrfxRD6u43ZeV2k5N.IfvfSu.n9N9j/Kr3HBHxMb6.La0sIS6' WHERE Email='loncocacola@gmail.com';
